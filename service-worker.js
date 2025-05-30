b8a7380efddf9677b0b51031285dbd8eadb0f4de
akzu07 <utria07101994@gmail.com>
2025-05-30 03:49:15 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const cacheName = 'beHappy-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+// Instala y guarda en cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Activa y limpia caches viejas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Intercepta peticiones y devuelve cache si hay
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
